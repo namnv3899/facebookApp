9c5246c98829c835c4bb2da5bcd2a938531af848
Ngoc Anh Pham <pnanh4@cmcglobal.vn>
2023-02-07 09:53:41 +0700

    chore: update post

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -2,13 +2,8 @@ const jwt = require("jsonwebtoken");
 const UserModel = require("../models/Users");
 const PostModel = require("../models/Posts");
 const FriendModel = require("../models/Friends");
-const DocumentModel = require("../models/Documents");
 var url = require("url");
 const httpStatus = require("../utils/httpStatus");
-const bcrypt = require("bcrypt");
-const { JWT_SECRET } = require("../constants/constants");
-const { ROLE_CUSTOMER } = require("../constants/constants");
-const uploadFile = require("../functions/uploadFile");
 
 const path = require("path");
 const postsController = {};
@@ -16,48 +11,6 @@ postsController.create = async (req, res, next) => {
   let userId = req.userId;
   try {
     const { described, images, videos } = req.body;
-    // let dataImages = [];
-    // if (Array.isArray(images)) {
-    //   for (const image of images) {
-    //     if (uploadFile.matchesFileBase64(image) !== false) {
-    //       const imageResult = uploadFile.uploadFile(image);
-    //       if (imageResult !== false) {
-    //         let imageDocument = new DocumentModel({
-    //           fileName: imageResult.fileName,
-    //           fileSize: imageResult.fileSize,
-    //           type: imageResult.type,
-    //         });
-    //         let savedImageDocument = await imageDocument.save();
-    //         if (savedImageDocument !== null) {
-    //           dataImages.push(savedImageDocument._id);
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
-
-    // let dataVideos = [];
-    // if (Array.isArray(videos)) {
-    //   for (const video of videos) {
-    //     console.log("video 1");
-    //     if (uploadFile.matchesFileBase64(video) !== false) {
-    //       console.log("video 2");
-    //       const videoResult = uploadFile.uploadFile(video);
-    //       if (videoResult !== false) {
-    //         console.log("video 3");
-    //         let videoDocument = new DocumentModel({
-    //           fileName: videoResult.fileName,
-    //           fileSize: videoResult.fileSize,
-    //           type: videoResult.type,
-    //         });
-    //         let savedVideoDocument = await videoDocument.save();
-    //         if (savedVideoDocument !== null) {
-    //           dataVideos.push(savedVideoDocument._id);
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
 
     const post = new PostModel({
       author: userId,
@@ -67,19 +20,7 @@ postsController.create = async (req, res, next) => {
       countComments: 0,
     });
     let postSaved = await post.save();
-    // postSaved = await PostModel.findById(postSaved._id)
-    //   .populate("images", ["fileName"])
-    //   .populate("videos", ["fileName"])
-    //   .populate({
-    //     path: "author",
-    //     select: "_id username phonenumber avatar",
-    //     model: "Users",
-    //     populate: {
-    //       path: "avatar",
-    //       select: "_id fileName",
-    //       model: "Documents",
-    //     },
-    //   });
+
     return res.status(httpStatus.OK).json({
       data: postSaved,
     });
@@ -124,8 +65,6 @@ postsController.edit = async (req, res, next) => {
 postsController.show = async (req, res, next) => {
   try {
     let post = await PostModel.findById(req.params.id)
-      // .populate("images", ["fileName"])
-      // .populate("videos", ["fileName"])
       .populate({
         path: "author",
         select: "_id username phonenumber avatar",
